eb2871dea5f0360c90f66846912236d698ce8e16
Jie Chen <jiechenmc@gmail.com>
2022-11-02 00:02:57 -0400

    Success on loading the latest term

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from playwright.async_api import async_playwright
+from core.login import login
 
 load_dotenv()
 
 
 async def run(playwright):
-    webkit = playwright.webkit
-    browser = await webkit.launch()
-    context = await browser.new_context()
-    page = await context.new_page()
+    browser = await playwright.chromium.launch()
+    page = await browser.new_page()
 
-    login = os.getenv("netid")
+    netid = os.getenv("netid")
     password = os.getenv("netid_password")
 
     await page.goto("https://classie-evals.stonybrook.edu/")
+
+    # Trigger Duo SSO
+    await login(page, netid, password)
+    await asyncio.sleep(5)
+
+    # Select the latest term
+    term_box = page.locator("#SearchTerm")
+    await term_box.select_option(index=1)
+
+    # Click The Go Button
+    await page.click("text=Go")
+
+    await page.wait_for_load_state('domcontentloaded')
     await page.screenshot(path="screenshot.png")
     await browser.close()
 
