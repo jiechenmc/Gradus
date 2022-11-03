3c8a28ba212c8a32d2e0f12343653a259e2f24a7
Jie Chen <jiechenmc@gmail.com>
2022-11-03 01:10:10 -0400

    script to migrate data.json to mongoDB

--- migrate.py
+++ migrate.py
@@ -1,10 +1,15 @@
+from json import loads
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
 # Mongo Setup
+load_dotenv()
 client = MongoClient(os.getenv("mongo_url"))
 db = client.get_database("Gradus")
 collection = db.get_collection("Spring 2022")
 
 with open("data.json", "r") as f:
-    pass
+    lines = f.readlines()
+    data = list(map(loads, lines))
+    collection.insert_many(data)
